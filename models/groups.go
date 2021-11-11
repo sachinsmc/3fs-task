8f0bdf6751895ce14a85c1eabd59967a96e78886
Sachin Chavan <chavansachin295@gmail.com>
2021-11-11 13:02:39 +0400

    format project code

--- models/groups.go
+++ models/groups.go
@@ -4,7 +4,7 @@ import "github.com/google/uuid"
 
 type Group struct {
 	ID        uuid.UUID `json:"id" sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	Name      string `json:"name"`
-	CreatedAt int64  `json:"created_at" gorm:"autoCreateTime" gorm:"<-:create"`
-	UpdatedAt int64  `json:"updated_at" gorm:"autoUpdateTime"`
+	Name      string    `json:"name"`
+	CreatedAt int64     `json:"created_at" gorm:"autoCreateTime" gorm:"<-:create"`
+	UpdatedAt int64     `json:"updated_at" gorm:"autoUpdateTime"`
 }
